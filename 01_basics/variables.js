91ce416994991dfd9adc7ef72bdacf2b6bab3340
mrdeerek <kunalraj845438@gmail.com>
2025-01-21 19:18:10 +0530

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "kunal@gmail.com"
+var accountPassword = "12345"
+accountCity = "bihar"
+let accountState;
+
+//accountId = 5  //not allowed
+ 
+
+
+accountEmail = "heic@gmail.com"
+accountPassword = "52525252"
+accountCity = "Patna"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState]);
