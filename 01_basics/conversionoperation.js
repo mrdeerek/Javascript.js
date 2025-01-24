ead8475b8b285d6615c90a32de5ad6225bc8eb47
mrdeerek <kunalraj845438@gmail.com>
2025-01-24 17:53:10 +0530

    add

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,30 @@
+//let score = 33;
+//let score ="33avd";
+//let score = undefined
+//let score = "Kunal"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33avd" => NaN
+// true => 1, false -=> 0
+
+//let IsLoggedIn = 1;
+// let IsLoggedIn = "Kunal"
+
+// let booleanIsLoggedIn = Boolean(IsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=> true, 0=> false
+// "" => false
+// "Kunal" => true
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+console.log(3 + 2);
+
+console.log("Kunal");
+
+let name = "Kunal";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+//number => 2 to power 53
+// Bigint
+// string =>""
+// boolean => true/false
+// null => standalonevalue
+// undefined
+// symbol => unique
+
+console.log(typeof "hitesh");
+console.log(typeof undefined);
